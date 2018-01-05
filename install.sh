9db2756b201c10d350823b8ec6e08081422d9e2a
Joe Samela <josephsamela@gmail.com>
2018-01-04 21:58:30 -0500

    updated installer to set everything up

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Make ~/.note directory
+mkdir "$HOME/.note"
+mkdir "$HOME/.note/archive"
+
+# Copy files to .note directory
+cp note.sh "$HOME/.note"
+cp readme.md "$HOME/.note"
+cp LICENSE "$HOME/.note"
+
 # Add "note" command to .bashrc
 echo '# Alias note commmand' >> "$HOME/.bashrc"
 echo 'alias note="bash ~/.note/note.sh"' >> "$HOME/.bashrc"
